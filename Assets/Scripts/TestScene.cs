bc94fe416d2ff395e40f5729b742c8f1c69ee08c
whoman4233 <62879651+whoman4233@users.noreply.github.com>
2025-08-17 09:43:53 +0900

    FINAL

--- Assets/Scripts/TestScene.cs
+++ Assets/Scripts/TestScene.cs
@@ -1,16 +1,17 @@
-//using UnityEngine;
+using UnityEngine;
 
-//public class TestScene : MonoBehaviour
-//{
-//    public UIManager ui;
-//    public DialogueLoader loader;
+public class TestScene : MonoBehaviour
+{
+    public UIManager ui;
+    public DialogueLoader loader;
+    public string DID;
 
-//    void Start()
-//    {
-//        DialogueData data = loader.GetDialogueById("intro01");
-//        if (data != null)
-//        {
-//            ui.ShowDialogueSequence(data, false); // false = 스페이스바 넘김
-//        }
-//    }
-//}
+    void Start()
+    {
+        DialogueData data = loader.GetDialogueByID(DID);
+        if (data != null)
+        {
+            ui.ShowDialogueSequence(data, false); // false = 스페이스바 넘김
+        }
+    }
+}
